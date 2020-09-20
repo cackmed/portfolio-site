ed7ed975c7e2a63c40844c58ba637f42e2d08082
Caleb Pendergraft <cackmed@gmail.com>
2020-09-20 07:05:46 -0700

    got most things working

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ let pageHeight, ticking, screenHeight, currentEl, pageSectionArr;
 
 function checkElement(scroll_pos) {
 
-  // how many times does pageHeight go into scrollPos
   let x = Math.floor(scroll_pos / (screenHeight / 2));
 
-  // set appropriate element if not already
   if(currentEl !== x){
     applyElement(x);
   }
@@ -27,8 +25,6 @@ function checkScroll(){
   }
 }
 
-// every time user scrolls, check if we need to change element visibility
-// seperating as we need to call checkScroll on page load too
 window.addEventListener('scroll', function() {
 
   checkScroll();
@@ -48,21 +44,16 @@ document.getElementById('icon').addEventListener('click', function() {
 
 
 
-// When the page is loaded
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Get height of screen
   screenHeight = screen.height;
 
-  // Get all page-sections on page and populate arrays
   pageSectionArr = document.getElementsByClassName('page-section');
 
-  // set total page height depending on number of projects
   pageHeight = screenHeight * pageSectionArr.length;
 
   document.getElementById('body').style.height = pageHeight / 2 + 'px';
 
-  // Check the scroll position, show appropriate project
   checkScroll();
 
 });
@@ -70,30 +61,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function applyElement(x){
 
-  // Set currentEl
   currentEl = x;
 
-  // for each project and project-img
-  // hide all
-  for(var i = pageSectionArr.length - 1; i >= 0; i--) {
-    // projectImgArr[i].style.display = "none"
+  for(let i = pageSectionArr.length - 1; i >= 0; i--) {
     pageSectionArr[i].classList.add('hidden');
   }
 
-  // Set background color on body
-  // get data-bgcolor attribute from .project-text element, white as fallback
   document.getElementById('body').style.background = pageSectionArr[x].dataset.bgcolor || 'white';
 
-  // Set text color on body, get it from data attribute, black as fallback
   document.getElementById('body').style.color = pageSectionArr[x].dataset.textcolor || 'black';
 
-  // Show appropriate project
   pageSectionArr[x].classList.remove('hidden');
   pageSectionArr[x].classList.add('shown');
 
 }
 
-// Function for handling smooth scrolling when user clicks down arrow
 document.getElementById('down-arrow').addEventListener('click', function(){
   window.scroll({
     top: (screenHeight / 2) + 100, 
@@ -102,11 +84,41 @@ document.getElementById('down-arrow').addEventListener('click', function(){
   });
 });
 
-// document.getElementById('down-arrow2').addEventListener('click', function(){
-//   window.scroll({
-//     top: (screenHeight / 2) + 600, 
-//     left: 0, 
-//     behavior: 'smooth' 
-//   });
-// });
+document.getElementById('home').addEventListener('click', function(){
+  window.scroll({
+    top: screenHeight - 1100, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
+});
+
+document.getElementById('projects').addEventListener('click', function(){
+  window.scroll({
+    top: (screenHeight / 2) + 100, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
+});
+
+document.getElementById('tech').addEventListener('click', function(){
+  window.scroll({
+    top: (screenHeight / 2) + 1700, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
+});
+
+document.getElementById('contact').addEventListener('click', function(){
+  window.scroll({
+    top: (screenHeight / 2) + 2200, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
+});
+
+
+
+
+
+
 
